f02defd56bf5e4173d133913d236542089a45eb6
Beyhan <30952126+StartaBafras@users.noreply.github.com>
2021-01-26 16:31:04 +0300

    Add files via upload

--- BYS.py
+++ BYS.py
@@ -1,6 +1,7 @@
-from PyQt5.QtWidgets import QTabWidget,QWidget,QApplication,QHBoxLayout,QMainWindow,QAction,QFormLayout,QDateEdit,QDateTimeEdit,QHeaderView
+from PyQt5.QtWidgets import QTabWidget,QWidget,QApplication,QHBoxLayout,QMainWindow,QAction,QFormLayout,QDateEdit,QDateTimeEdit,QHeaderView,QDateTimeEdit
 from PyQt5.QtWidgets import QLabel,QLineEdit,QRadioButton,QPushButton,QMessageBox,QSpinBox,QVBoxLayout,QComboBox,QSpinBox,QTableWidget,QTableWidgetItem
 import sqlite3
+from PyQt5.QtCore import QDate,QDateTime
 import sys
 import data
 #Hizmetler:
@@ -91,6 +92,9 @@ class add_order(QWidget):
         self.order_amount = QLabel("Miktar")
         self.order_amount_i = QSpinBox()
 
+        self.order_date=QLabel("Sipariş Tarihi")
+        self.order_date_i=QDateTimeEdit(QDateTime.currentDateTime())
+
         self.save_button = QPushButton("Kayıt Et")
         self.save_button.clicked.connect(self.save)
 
@@ -105,6 +109,8 @@ class add_order(QWidget):
         f_box.addWidget(self.order_owner_i)
         f_box.addWidget(self.order_amount)
         f_box.addWidget(self.order_amount_i)
+        f_box.addWidget(self.order_date)
+        f_box.addWidget(self.order_date_i)
         f_box.addWidget(self.save_button)
         
 
@@ -117,10 +123,11 @@ class add_order(QWidget):
             o_file = self.order_file_i.text()
             owner = self.order_owner_i.text()
             amount = self.order_amount_i.text()
+            date = self.order_date_i.text()
             print(name,o_file,owner,amount)
             con=sqlite3.connect("printer.db")
             cursor=con.cursor()
-            cursor.execute("insert into orders values(?,?,?,?)",(name,o_file,owner,amount))
+            cursor.execute("insert into orders values(?,?,?,?,?)",(name,o_file,owner,amount,date))
             con.commit()
             con.close()
             QMessageBox.about(self,"Veritabanı","Kayıt Edildi")
@@ -131,10 +138,11 @@ class see_order(QWidget):
     def __init__(self):
         super().__init__()
         f_box = QFormLayout()
+        h_box = QHBoxLayout()
 
         self.table = QTableWidget()
         #self.table.setRowCount(4)
-        self.table.setColumnCount(4)
+        self.table.setColumnCount(5)
 
         self.table.horizontalHeader().setStretchLastSection(True) 
         self.table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
@@ -142,23 +150,47 @@ class see_order(QWidget):
         self.load_button = QPushButton("Veritabanından Yükle")
         self.load_button.clicked.connect(self.load)
 
+        self.delete_button = QPushButton("Sil")
+        self.delete_button.clicked.connect(self.delete)
 
 
 
 
-        f_box.addWidget(self.load_button)
+        h_box.addWidget(self.load_button)
+        h_box.addWidget(self.delete_button)
+
+        
         f_box.addWidget(self.table)
-
+        f_box.addItem(h_box)
         self.setLayout(f_box)
 
     def load(self):
-        con = sqlite3.connect("printer.db")
-        raw_data = con.execute("SELECT * FROM orders")
-        self.table.setRowCount(0)
-        for row_number, row_data in enumerate(raw_data):
-            self.table.insertRow(row_number)
-            for column_number, data in enumerate(row_data):
-                self.table.setItem(row_number,column_number,QTableWidgetItem(str(data)))
+        try:
+            con = sqlite3.connect("printer.db")
+            raw_data = con.execute("SELECT * FROM orders")
+            self.table.setRowCount(0)
+            for row_number, row_data in enumerate(raw_data):
+                self.table.insertRow(row_number)
+                for column_number, data in enumerate(row_data):
+                    self.table.setItem(row_number,column_number,QTableWidgetItem(str(data)))
+            con.close()
+        except sqlite3.OperationalError:
+            QMessageBox.about(self,"sqlite3.OperationalError","Veritabanına bağlanılamadı lütfen yeniden oluşturmayı deneyin veya geliştirici ile iletişime geçin")
+
+    def delete(self):
+        try:
+            #a = self.table.currentRow()#factmany ile kullanılabilir veritabanında benzersiz ıd olmalı sanırım
+            c = self.table.item(self.table.currentRow(),4).text()
+            con = sqlite3.connect("printer.db")
+            cursor = con.cursor()
+            cursor.execute("DELETE FROM orders WHERE date = ?",[c])
+            con.commit()
+            con.close()
+            self.load()
+        except sqlite3.OperationalError:
+            QMessageBox.about(self,"sqlite3.OperationalError","Veritabanına bağlanılamadı lütfen yeniden oluşturmayı deneyin veya geliştirici ile iletişime geçin")
+
+        
 
 
 
--- data.py
+++ data.py
@@ -2,7 +2,7 @@ import sqlite3
 def create():
     con=sqlite3.connect("printer.db")
     cur=con.cursor()
-    cur.execute("create table if not exists orders('name','file','owner','amount')")
-    cur.execute("insert into orders values(?,?,?,?)",('İsim','Dosya','Alıcı','Miktar'))
+    cur.execute("create table if not exists orders('name','file','owner','amount','date')")
+    cur.execute("insert into orders values(?,?,?,?,?)",('İsim','Dosya','Alıcı','Miktar','Tarih'))
     con.commit()
     con.close()
